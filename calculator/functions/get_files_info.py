8cc57a64d2941ad490f02f37796546b92bdb2e5e
Jay <jay_en08@yahoo.com>
2026-01-01 15:24:04 -0500

    initial commit

---
+++ calculator/functions/get_files_info.py
@@ -0,0 +1,31 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    try:
+        contents = []
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        
+        for item in os.listdir(target_dir):
+            item_path = os.path.join(target_dir, item)
+            
+            if os.path.isdir(item_path):
+                file_size = os.path.getsize(item_path)
+                is_dir = True
+            else:
+                file_size = os.path.getsize(item_path)
+                is_dir = False
+            
+            item_info = f"- {item}: file_size={file_size} bytes, is_dir={is_dir}"
+            contents.append(item_info)
+
+    except Exception as e:
+        return f"Error listing files: {e}"
+    return "\n".join(contents)

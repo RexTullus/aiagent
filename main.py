850488c359ff333f282fd687187cde9ab82e06c1
Jay <jay_en08@yahoo.com>
2026-01-05 12:00:58 -0500

    Ch4 L1 Updates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 import argparse
@@ -20,29 +21,46 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]  
     
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(
-    model='gemini-2.5-flash', contents=messages, 
-    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0.0),
-    )
 
-    if response.usage_metadata is not None and args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    for _ in range(20):
+        response = client.models.generate_content(
+        model='gemini-2.5-flash', contents=messages, 
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0.0),
+        )
 
-    if response.function_calls:
-        for item in response.function_calls:
-            function_call_result = call_function(item, args.verbose)
-            if function_call_result.parts:
-                if function_call_result.parts[0].function_response:
-                    if args.verbose:
-                        print(f"-> {function_call_result.parts[0].function_response.response}")
+
+        if response.usage_metadata is not None and args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+            
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+
+        if response.function_calls:
+            #function_responses = []
+            for item in response.function_calls:
+                function_call_result = call_function(item, args.verbose)
+                if function_call_result.parts:
+                    if function_call_result.parts[0].function_response:
+                        if args.verbose:
+                            print(f"-> {function_call_result.parts[0].function_response.response}")
+                            #function_responses.append(types.Part.from_function_response(function_call_result.parts[0].function_response))
+                        messages.append(function_call_result)
+                    else:
+                        raise Exception("function_call_result.parts[0].function_response.response empty")
                 else:
-                    raise Exception("function_call_result.parts[0].function_response.response empty")
-            else:
-                 raise Exception("function_call_result.parts returned no data")
+                    raise Exception("function_call_result.parts returned no data")
+            #messages.append(types.Content(role="user", parts=function_responses))
+        else:
+                print(response.text)
+                break
+           
     else:
-            print(response.text)
+        print("ERROR: maximum loops without completed response", file=sys.stderr)
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
+
